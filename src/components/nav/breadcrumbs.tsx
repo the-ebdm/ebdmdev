9505b919f0fa54809240e971e18c626a7d19f556
Eric Muir <eric@bossk.ebdm.dev>
2023-09-06 13:17:39 +0100

    :sparkles: Added floating Breadcrumbs

--- src/components/nav/breadcrumbs.tsx
+++ src/components/nav/breadcrumbs.tsx
@@ -1,12 +1,17 @@
 import html from '@kitajs/html'
 
-export default function Breadcrumbs({ breadcrumbs }: { breadcrumbs: any[] }) {
+interface Breadcrumb {
+  name: string;
+  href: string;
+}
+
+export default function Breadcrumbs({ breadcrumbs }: { breadcrumbs: Breadcrumb[] }) {
   return (
-    <nav class="flex" aria-label="Breadcrumb">
-      <ol role="list" class="flex space-x-4 rounded-md bg-white px-6 shadow">
+    <nav class="flex absolute top-5 left-5" aria-label="Breadcrumb">
+      <ol hx-boost="true" role="list" class="flex space-x-4 rounded-md bg-white px-6 shadow">
         <li class="flex">
           <div class="flex items-center">
-            <a href="#" class="text-gray-400 hover:text-gray-500">
+            <a href="/" class="text-gray-400 hover:text-gray-500">
               {`<svg class="h-5 w-5 flex-shrink-0" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                 <path fill-rule="evenodd" d="M9.293 2.293a1 1 0 011.414 0l7 7A1 1 0 0117 11h-1v6a1 1 0 01-1 1h-2a1 1 0 01-1-1v-3a1 1 0 00-1-1H9a1 1 0 00-1 1v3a1 1 0 01-1 1H5a1 1 0 01-1-1v-6H3a1 1 0 01-.707-1.707l7-7z" clip-rule="evenodd" />
               </svg>`}
@@ -14,22 +19,21 @@ export default function Breadcrumbs({ breadcrumbs }: { breadcrumbs: any[] }) {
             </a>
           </div>
         </li>
-        <li class="flex">
-          <div class="flex items-center">
-            {`<svg class="h-full w-6 flex-shrink-0 text-gray-200" viewBox="0 0 24 44" preserveAspectRatio="none" fill="currentColor" aria-hidden="true">
-              <path d="M.293 0l22 22-22 22h1.414l22-22-22-22H.293z" />
-            </svg>`}
-            <a href="#" class="ml-4 text-sm font-medium text-gray-500 hover:text-gray-700">Projects</a>
-          </div>
-        </li>
-        <li class="flex">
-          <div class="flex items-center">
-            {`<svg class="h-full w-6 flex-shrink-0 text-gray-200" viewBox="0 0 24 44" preserveAspectRatio="none" fill="currentColor" aria-hidden="true">
-              <path d="M.293 0l22 22-22 22h1.414l22-22-22-22H.293z" />
-            </svg>`}
-            <a href="#" class="ml-4 text-sm font-medium text-gray-500 hover:text-gray-700" aria-current="page">Project Nero</a>
-          </div>
-        </li>
+        {breadcrumbs.map((breadcrumb: Breadcrumb) => (
+          <li class="flex">
+            <div class="flex items-center">
+              {`<svg class="h-full w-6 flex-shrink-0 text-gray-200" viewBox="0 0 24 44" preserveAspectRatio="none" fill="currentColor" aria-hidden="true">
+                <path d="M.293 0l22 22-22 22h1.414l22-22-22-22H.293z" />
+              </svg>`}
+              <a
+                href={breadcrumb.href}
+                style="max-width: 10rem;"
+                class="ml-4 text-sm font-medium text-gray-500 hover:text-gray-700 truncate text-ellipsis">
+                {breadcrumb.name}
+              </a>
+            </div>
+          </li>
+        ))}
       </ol>
     </nav>
   )
