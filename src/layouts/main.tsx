0ac3cad8d6ea2e0ab9f4d4e7b8374be7e5b93ade
Eric Muir <eric@bossk.ebdm.dev>
2023-09-06 15:19:27 +0100

    🧑‍💻 Migrate string to html

--- src/layouts/main.tsx
+++ src/layouts/main.tsx
@@ -2,25 +2,30 @@ import html from '@kitajs/html'
 import { Attributes } from "typed-html"
 
 const BaseHtml = ({ children, ...attributes }: Attributes) => {
-	const { title } = attributes;
-	return `<!DOCTYPE html>
-	<html lang="en" class="h-full bg-gray-100">
-		<head>
-			<meta charset="utf-8" />
-			<meta name="viewport" content="width=device-width" />
-			<link rel="icon" type="image/svg+xml" href="/favicon.svg" />
-			<title>${title}</title>
-			<link href="/styles.css" rel="stylesheet">
-		</head>
-		<body class="h-full">
-			<!-- TODO: Light/Dark switcher -->
-			${children}
+	const { title, navigation } = attributes;
+	return (
+		<>
+			{`<!DOCTYPE html>`}
+			<html lang="en" class="h-full bg-gray-100">
+				<head>
+					<meta charset="utf-8" />
+					<meta name="viewport" content="width=device-width" />
+					<link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+					<title>${title}</title>
+					<link href="/styles.css" rel="stylesheet" />
+				</head>
+				<body class="h-full">
+					{/* TODO: Light/Dark switcher */}
 
-			<script src="https://unpkg.com/htmx.org@1.9.5"></script>
-			<script src="https://unpkg.com/hyperscript.org@0.9.11"></script>
-			<script src="https://code.iconify.design/3/3.1.1/iconify.min.js"></script>
-		</body>
-	</html>`
+					{children}
+
+					<script src="https://unpkg.com/htmx.org@1.9.5"></script>
+					<script src="https://unpkg.com/hyperscript.org@0.9.11"></script>
+					<script src="https://code.iconify.design/3/3.1.1/iconify.min.js"></script>
+				</body>
+			</html>
+		</>
+	)
 };
 
 export default BaseHtml;
