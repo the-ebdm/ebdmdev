a616e3ac1dfa4689e1faf1126dfa11eaca714acd
Eric Muir <eric@bossk.ebdm.dev>
2023-09-07 11:38:14 +0100

    ⚡️ Reduce data to page load

--- src/routes/blog/index.tsx
+++ src/routes/blog/index.tsx
@@ -3,12 +3,12 @@ import html from '@kitajs/html'
 import BlogLayout from "@layouts/blog";
 import Error from '@components/error'
 import List from "@components/blog/list";
-import { getPublishedArticlesWithBlocks } from "src/lib/notion";
+import { getPublishedArticles } from "src/lib/notion";
 import { isHTMX } from "src/lib/html";
 
 export const get = async ({ request, set }: any) => {
   const headers = request.headers as Headers;
-  const posts = await getPublishedArticlesWithBlocks();
+  const posts = await getPublishedArticles();
   // If the request is from a browser, and the request is not boosted, return the list of posts.
   try {
     if (isHTMX(headers)) {
