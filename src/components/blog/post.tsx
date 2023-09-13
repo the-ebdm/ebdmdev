ff35afe9d691dd3afba464380ae2b7e3499281fa
Eric Muir <eric@bossk.ebdm.dev>
2023-09-13 15:37:35 +0100

    💄 Fix blog post date render

--- src/components/blog/post.tsx
+++ src/components/blog/post.tsx
@@ -5,10 +5,11 @@ import Breadcrumbs from '@components/nav/breadcrumbs';
 
 export default function Post({ post }: { post: any }) {
   const blocks = post.blocks as any[];
+  const date = new Date(post.created_time).toLocaleDateString('en-GB', {});
   return (
     <article class="flex max-w-xl flex-col items-start justify-between bg-white p-4 rounded-md">
       <div class="flex items-center gap-x-4 text-xs" hx-boost="true">
-        <time datetime="2020-03-16" class="text-gray-500">Mar 16, 2020</time>
+        <time datetime={post.created_time} class="text-gray-500">{date}</time>
         {/* TODO: Implement tags */}
         {/* <a href={`/blog/${post.id}`} class="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100">Marketing</a> */}
       </div>
