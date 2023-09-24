b26bbe77c54948a399912a774ae26a4ebd3c4d3f
Eric Muir <eric@bossk.ebdm.dev>
2023-09-24 14:06:59 +0100

    💄 Add basic sign in and up components

--- src/components/auth/signIn.tsx
+++ src/components/auth/signIn.tsx
@@ -1,31 +1,49 @@
 import html from '@kitajs/html'
 
-import Layout from '@layouts/main'
-
 export default function SignIn() {
   return (
-    <Layout title="EBDM.DEV / Sign In">
-      <div id="login"></div>
-      <script
-        async
-        crossorigin="anonymous"
-        data-clerk-publishable-key={process.env.CLERK_PUB_KEY}
-        src={process.env.CLERK_URL}
-        type="text/javascript">
-      </script>
-      {`<script>
-      window.addEventListener('load', async function () {
-        const login = document.getElementById('login');
-        await window.Clerk.load();
-        window.Clerk.openSignIn(login, {
-          appearance: {
-            variables: {
-              colorPrimary: "#14B8A6",
-            }
-          }
-        })
-      });
-      </script>`}
-    </Layout>
+    <div id="container" class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8" style="view-transition-name: slide-it-left;">
+      <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+        <img class="mx-auto h-10 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=teal&shade=600" alt="Your Company" />
+        <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Sign in to your account</h2>
+      </div>
+
+      <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <form class="space-y-6" action="/app/auth/signin" method="POST">
+          <div>
+            <label for="username" class="block text-sm font-medium leading-6 text-gray-900">Username or Email address</label>
+            <div class="mt-2">
+              <input id="username" name="username" type="text" autocomplete="username" required="true" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary-600 sm:text-sm sm:leading-6" />
+            </div>
+          </div>
+
+          <div>
+            <div class="flex items-center justify-between">
+              <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Password</label>
+              <div class="text-sm">
+                <a href="#" class="font-semibold text-primary-600 hover:text-primary-500">Forgot password?</a>
+              </div>
+            </div>
+            <div class="mt-2">
+              <input id="password" name="password" type="password" autocomplete="current-password" required="true" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary-600 sm:text-sm sm:leading-6" />
+            </div>
+          </div>
+
+          <div>
+            <button type="submit" class="flex w-full justify-center rounded-md bg-primary-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-primary-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-600">Sign in</button>
+          </div>
+        </form>
+
+        <p class="mt-10 text-center text-sm text-gray-500">
+          Not a member?&nbsp;
+          <a
+            hx-get="/app/auth/signup"
+            hx-swap="innerHTML transition:true"
+            hx-target="#container"
+            hx-push-url="true"
+            class="font-semibold leading-6 text-primary-600 hover:text-primary-500">Start a 14 day free trial</a>
+        </p>
+      </div>
+    </div>
   )
 }
