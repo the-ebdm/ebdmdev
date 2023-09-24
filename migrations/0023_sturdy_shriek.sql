32a4c945fe474900a585508c4d19ada28946f404
Eric Muir <eric@bossk.ebdm.dev>
2023-09-24 14:30:39 +0100

    🗃️ Fix user tokens

---
+++ migrations/0023_sturdy_shriek.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS "user_tokens" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"user_id" integer NOT NULL,
+	"token" varchar NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"expires_at" timestamp NOT NULL,
+	CONSTRAINT "user_tokens_token_unique" UNIQUE("token")
+);
